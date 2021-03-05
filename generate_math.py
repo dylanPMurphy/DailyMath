89489146b9198d773b02141ba58e342cc4da437e
mac <dylanmurphyDBA@gmail.com>
2021-03-05 00:33:54 -0800

    problem generator works with hashed answers!

--- generate_math.py
+++ generate_math.py
@@ -1,11 +1,13 @@
 # How do we generate 10 random math problems for addition subtraction multiplication and division
+import random
+
 
 class Problem:
     def __init__(self, operator, operand_a, operand_b):
         self.operator = operator
         self.operand_a = operand_a
         self.operand_b = operand_b
-        self.result = self.getResult()
+        self.result = hash(str(self.getResult()))
     
     def getResult(self):
         if self.operator is '+':
@@ -21,9 +23,9 @@ class Problem:
 
 
     def __str__(self):
-        return str(self.operand_a) +' '+ self.operator +' '+ str(self.operand_b) +' = '+str(self.result)
+        return str(self.operand_a) +' '+ self.operator +' '+ str(self.operand_b) +' = '+str(self.result) +  '\n'
 
-
+print("*****SIMPLE TEST*******\n")
 print("Addition:")
 problem1 = Problem('+', 2, 2)
 print(problem1)
@@ -39,3 +41,16 @@ print(problem3)
 print("Division:")
 problem4 = Problem('/', 100, 12)
 print(problem4)
+print("******************\n\n")
+
+operators = ['+','-','*','/']
+print(random.randint(0,3))
+print(random.randint(0,3))
+print(random.randint(0,3))
+
+print(random.randint(0,3))
+
+random_problems = []
+for i in range(10):
+    random_problems.append(Problem(operators[random.randint(0,3)], random.randrange(100), random.randrange(100)))
+    print(random_problems[i])
